c9d60c5863d385ed95726a25cb0c6dec48b3114f
mhuwiler <marc.huwiler@cern.ch>
2020-05-04 17:22:30 +0200

    First version working on lxplus with HTCondor

--- Skimmer/newsubmission/executable.sh
+++ Skimmer/newsubmission/executable.sh
@@ -4,11 +4,28 @@ echo $FILE_NAMES
 
 env
 
+
+WD=/afs/cern.ch/work/m/mhuwiler/software/cc7/Wtagging/CMSSW_10_2_6/src/WTopScalefactorProducer/Skimmer/newsubmission
+
+cd $WD
+
+#export SCRAM_ARCH=slc7_amd64_gcc700           #### Better to work on CC7 than SL6
+#export X509_USER_PROXY=${HOME}/private/x509up
+
+source env.sh
+
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+cd ${CMSSW_BASE}/src
+eval `scramv1 runtime -sh`
+
+cd $WD
+
+
 python $CMSSW_BASE/src/WTopScalefactorProducer/Skimmer/newsubmission/ProductionScript.py "$GC_SCRATCH" "$FILE_NAMES" 
 
 # Additional manual copying of file for safety
 # /scratch/mhuwiler/Wtagging/production/${GC_TASK_ID}/${DATASETPATH}/
-destinationpath=/work/mhuwiler/data/WScaleFactors/production/${GC_TASK_ID}/${DATASETPATH}/
+destinationpath=/eos/home-m/mhuwiler/data/Wtagging/production/${GC_TASK_ID}/${DATASETPATH}/
 
 mkdir -p $destinationpath
 
