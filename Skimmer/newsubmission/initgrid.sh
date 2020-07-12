955c695507e9bf97cf4035dae79468d0ce789148
mhuwiler <marc.huwiler@cern.ch>
2020-07-12 15:48:49 +0200

    Some fixes to make it more path independent

--- Skimmer/newsubmission/initgrid.sh
+++ Skimmer/newsubmission/initgrid.sh
@@ -1,2 +1,6 @@
+mkdir -p ${HOME}/private/voms
+export X509_USER_PROXY=${HOME}/private/voms/x509_voms
 voms-proxy-init  --voms cms --valid 200:00:00
-export PATH=$PATH:$CMSSW_BASE/src/WTopScalefactorProducer/Skimmer/grid-control:$CMSSW_BASE/src/WTopScalefactorProducer/Skimmer/grid-control/scripts
+GRIDCONTROLDIR=($CMSSW_BASE/src/*/Skimmer/grid-control)
+PATHADDITION=$GRIDCONTROLDIR:$GRIDCONTROLDIR/scripts
+export PATH=$PATH:$PATHADDITION
